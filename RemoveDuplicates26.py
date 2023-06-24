098b05c713ea6130d83356197dd2baf8974ad2c1
John8445 <halljohn3366@gmail.com>
2023-06-24 00:14:18 -0400

    Add solution code for the remove duplicates in array stable problem

---
+++ RemoveDuplicates26.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        k = 0
+      
+        for i in range(len(nums)):
+            
+            if nums[k] < nums[i]:
+                nums[k+1] = nums[i]
+                k+=1
+            
+        return k + 1
